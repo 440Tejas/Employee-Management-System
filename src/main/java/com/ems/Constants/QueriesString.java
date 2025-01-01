aa91597647791748b42f2c23db035c77d0f34d46
440Tejas <105191764+440Tejas@users.noreply.github.com>
2025-01-01 22:32:41 +0530

    Update QueriesString.java

--- src/main/java/com/ems/Constants/QueriesString.java
+++ src/main/java/com/ems/Constants/QueriesString.java
@@ -3,5 +3,5 @@ package com.ems.Constants;
 
 public class QueriesString {
 
-	StringBuffer query = new StringBuffer("Insert Into Region(name) value(?)");
+	StringBuffer query = new StringBuffer("Insert Into Region(name) value(?)"); 
 }
